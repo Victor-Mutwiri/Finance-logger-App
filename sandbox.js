8c7bb998615017b4709115758c1f72232805cbd5
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-02-03 23:58:16 +0300

    1.1

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,7 @@
-var character = 'mario';
-console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var characters = 'james';
+var age = 30;
+var isBlackBelt = false;
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(7));
---
+++ sandbox.ts
@@ -0,0 +1,12 @@
+let characters = 'james'
+
+let age =30;
+
+let isBlackBelt = false
+
+
+const circ =(diameter:number)=>{
+    return diameter * Math.PI;
+}
+
+console.log(circ(7))
