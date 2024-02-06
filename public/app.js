5c106c883fd5e7f0cbd9d40fbc2648c425be712f
Victor-Mutwiri <mutwirilincoln19@gmail.com>
2024-02-06 14:56:11 +0300

    1.6

--- public/app.js
+++ public/app.js
@@ -1,40 +1,49 @@
 import { Payment } from './classes/payment.js';
 import { Invoice } from './classes/invoice.js';
-/* let docOne:HasFormatter;
-let docTwo:HasFormatter;
-
-docOne = new Invoice('yoshi','web work', 2500)
-docTwo = new Payment('mario','plumbing', 800)
-
-let docs: HasFormatter[] = []
-docs.push(docOne)
-docs.push(docTwo)
-
-console.log(docs)
-
-const invOne = new Invoice('mario','work on the mario website', 2500)
-const invTwo = new Invoice('Tito','work on the Tito website', 1500)
-
-let invoices:Invoice[] = []
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv=>{
-    console.log(inv.client, inv.amount, inv.format())
-}) */
+import { ListTemplate } from './classes/listTemplate.js';
 const form = document.querySelector('.new-item-form');
+//inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
+//ENUMS
+/* enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+
+interface Resource <T>{
+    uid:number;
+    resourceName:ResourceType;
+    data: T
+}
+const docOne: Resource <object> = {
+    uid: 3,
+    resourceName: ResourceType.BOOK,
+    data: {name: 'Pascal'}
+}
+
+const docTwo: Resource<String[]>={
+    uid: 2,
+    resourceName: ResourceType.PERSON,
+    data:['bread', 'milk', 'toiletroll']
+}
+
+console.log(docOne, docTwo) */
+//Tuples
+let arr = ['ryu', 25, true];
+let tup = ['ryu', 25, true];
--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,62 @@
 import {Payment} from './classes/payment.js'
 import {Invoice} from './classes/invoice.js'
 import {HasFormatter} from './interfaces/hasformatter.js'
-
-/* let docOne:HasFormatter;
-let docTwo:HasFormatter;
-
-docOne = new Invoice('yoshi','web work', 2500)
-docTwo = new Payment('mario','plumbing', 800)
-
-let docs: HasFormatter[] = []
-docs.push(docOne)
-docs.push(docTwo)
-
-console.log(docs)
-
-const invOne = new Invoice('mario','work on the mario website', 2500)
-const invTwo = new Invoice('Tito','work on the Tito website', 1500)
-
-let invoices:Invoice[] = []
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach(inv=>{
-    console.log(inv.client, inv.amount, inv.format())
-}) */
+import { ListTemplate } from './classes/listTemplate.js';
 
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
+//inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
 
+//list template instance
+const ul = document.querySelector('ul')!
+const list = new ListTemplate(ul)
+
 form.addEventListener('submit', (e:Event)=>{
     e.preventDefault();
+
+    let values: [string, string, number]
+    values= [tofrom.value, details.value, amount.valueAsNumber]
+
     let doc: HasFormatter;
     if (type.value === 'invoice'){
-        doc = new Invoice(tofrom.value, details.value,amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(tofrom.value, details.value,amount.valueAsNumber)
+        doc = new Payment(...values)
     }
-    console.log(doc)
+    
+    list.render(doc,type.value, 'end')
 })
+
+
+//ENUMS
+/* enum ResourceType {BOOK, AUTHOR, FILM, DIRECTOR, PERSON}
+
+interface Resource <T>{
+    uid:number;
+    resourceName:ResourceType;
+    data: T
+}
+const docOne: Resource <object> = {
+    uid: 3,
+    resourceName: ResourceType.BOOK,
+    data: {name: 'Pascal'}
+}
+
+const docTwo: Resource<String[]>={
+    uid: 2,
+    resourceName: ResourceType.PERSON,
+    data:['bread', 'milk', 'toiletroll']
+}
+
+console.log(docOne, docTwo) */
+
+//Tuples
+
+let arr = ['ryu', 25, true];
+
+let tup: [string,number,boolean] = ['ryu', 25, true]
